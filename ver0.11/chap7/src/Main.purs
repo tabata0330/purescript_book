6d34ea3860b8abf004cdd4326c16de6667b907af
tabata0330 <tabata@db.ics.keio.ac.jp>
2019-03-14 00:50:16 +0900

    finish 7.10

--- ver0.11/chap7/src/Main.purs
+++ ver0.11/chap7/src/Main.purs
@@ -1,14 +1,22 @@
 module Main where
 
 import Control.Applicative
+import Data.AddressBook
 import Data.List
 import Data.Maybe
+import Data.String
+import Data.String.Regex
+import Data.Validation.Semigroup
 import Prelude
 
 import Control.Apply (lift2, (<*>))
 import Data.AddressBook (fullName)
 import Data.Either (Either(..))
 import Data.Functor ((<$>))
+import Data.Int (radix)
+import Data.String.CodePoints (length)
+import Data.String.Regex.Flags (RegexFlags(..), noFlags)
+import Partial.Unsafe (unsafePartial)
 
 withError :: (Maybe String) -> String -> (Either String String)
 withError Nothing err = Left err
@@ -32,3 +40,58 @@ optionalAdd a b = apply (map add a) b
 combineMaybe :: forall a f. (Applicative f) => Maybe (f a) -> f (Maybe a)
 combineMaybe (Just f) = Just <$> f
 combineMaybe Nothing = pure Nothing
+
+type Errors = Array String
+
+lengthIs :: String -> Int -> String -> V Errors Unit
+lengthIs field len value | length value /= len =
+  invalid ["Field '" <> field <> "' must have length " <> show len]
+lengthIs _ _ _ =
+  pure unit
+
+phoneNumberRegex :: Regex
+phoneNumberRegex =
+  unsafePartial
+    case regex "^\\d{3}-\\d{3}-\\d{4}$" (RegexFlags { unicode: false, sticky: false, multiline: false, ignoreCase: false, global: false}) of
+      Right r -> r
+
+matches :: String -> Regex -> String -> V Errors Unit
+matches _ regex value | test regex value =
+  pure unit
+matches field _ _ =
+  invalid ["Field '" <> field <> "' did not match the required format"]
+
+validatePhoneNumber :: PhoneNumber -> V Errors PhoneNumber
+validatePhoneNumber (PhoneNumber o) =
+  phoneNumber <$> pure o."type"
+              <*> (matches "Number" phoneNumberRegex o.number *> pure o.number)
+
+-- 演習7.10-1
+stateRegex :: Regex
+stateRegex = 
+  unsafePartial
+    case regex "^[a-zA-Z]{2}$" noFlags of
+      Right r -> r
+
+validateState :: Address -> V Errors Address
+validateState (Address o) =
+  address <$> pure o.street
+          <*> pure o.city
+          <*> (matches "State" stateRegex o.state *> pure o.state)
+
+-- 演習7.10-2
+blankRegex :: Regex
+blankRegex = 
+  unsafePartial
+    case regex "^\\S+$" noFlags of
+      Right r -> r
+
+nonEmpty :: String -> String -> V Errors Unit
+nonEmpty field "" = invalid ["Field '" <> field <> "' cannot be empty"]
+nonEmpty _     _  = pure unit
+
+validateAddress :: Address -> V Errors Address
+validateAddress (Address o) =
+  address <$> (matches "Street" blankRegex o.street  *> pure o.street)
+          <*> (matches "City" blankRegex   o.city    *> pure o.city)
+          <*> (matches "State" stateRegex o.state *> pure o.state)
